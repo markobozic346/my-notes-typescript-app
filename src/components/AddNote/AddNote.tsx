07b0c1c5207775020103eb45ccb807b8d3fd90fa
markobozic346 <55995892+markobozic346@users.noreply.github.com>
2021-04-10 09:11:23 +0200

    added add note dialog

--- src/components/AddNote/AddNote.tsx
+++ src/components/AddNote/AddNote.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import {makeStyles} from '@material-ui/styles';
-import {Button} from '@material-ui/core';
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/styles';
+import { Button, TextField } from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 const useStyles = makeStyles({
     addNote: {
-        
+
         position: 'sticky',
         bottom: '10px',
         right: '10px',
         float: 'right'
-        
+
     },
     plusIcon: {
         margin: '0',
@@ -23,18 +29,47 @@ const useStyles = makeStyles({
         '&:hover': {
             backgroundColor: '#b0a80c',
         }
+    },
+    dialogBox: {
+        color: '#FFFF99',
     }
 })
 interface Props {
-    
+
 }
 
-const AddNote = (props: Props) => {
+const AddNote: React.FC = (props: Props) => {
+
+    const [open, setOpen] = useState<true | false>(false)
     const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
     return (
         <div className={classes.addNote}>
-            <Button className={classes.plusIcon}>➕</Button>
-            
+            {/* on click opens dialog box */}
+            <Button className={classes.plusIcon} onClick={handleOpen}>➕</Button>
+            <Dialog open={open} >
+                <DialogTitle>Add New Note</DialogTitle>
+                <DialogContent >
+                    <DialogContentText>Ovde ide nesto ne znam sta</DialogContentText>
+
+                    <TextField label='note title' />
+                    <TextField label='tags' />
+                    <TextField label='description' />
+                </DialogContent>
+
+                <DialogActions>
+                    <Button>Edit</Button>
+                    <Button onClick={handleClose}>Close</Button>
+                </DialogActions>
+
+            </Dialog>
+
         </div>
     )
 }
